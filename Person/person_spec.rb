80b3d5031bb8ce53e4c60737c55c845be8d5210f
tinasoul <tinapshah@gmail.com>
2013-07-30 09:53:55 -0500

    Create let method for repeat code.

--- Person/person_spec.rb
+++ Person/person_spec.rb
@@ -3,9 +3,10 @@ require 'rspec'
 require_relative 'person'
 
 describe Person do
+  let("person") { Person.new "Matt", "Buck" }
   describe ".initialize" do
     it "creates a new person with a first and last name" do
-      person = Person.new "Matt", "Buck"
+      # person = Person.new "Matt", "Buck"
       expect(person.first_name).to eq("Matt")
       expect(person.last_name).to eq("Buck")
     end
@@ -13,7 +14,7 @@ describe Person do
 
 describe "#full_name" do
   it "returns the full name of the person" do
-    person = Person.new "Matt", "Buck"
+    # person = Person.new "Matt", "Buck"
     expect(person.full_name).to eq("Matt Buck")
    end
   end
