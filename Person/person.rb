a10bfe0c92b3761cedb9d6a5042ef4128aab0b36
tinasoul <tinapshah@gmail.com>
2013-07-30 10:30:05 -0500

    Created reverse_name method.

--- Person/person.rb
+++ Person/person.rb
@@ -1,13 +1,17 @@
 class Person
   attr_accessor :first_name, :last_name
 
-  def initialize(first_name, last_name)
+  def initialize(first_name = nil, last_name = nil)
     @first_name= first_name
     @last_name= last_name
   end  
 
   def full_name
     # first_name + " " + last_name
-    "#{first_name} #{last_name}"
+    "#{first_name} #{last_name}" if first_name || last_name
+  end
+
+  def reverse_name
+    "#{first_name} #{last_name}".reverse
   end
 end
--- Person/person_spec.rb
+++ Person/person_spec.rb
@@ -3,22 +3,50 @@ require 'rspec'
 require_relative 'person'
 
 describe Person do
-  let("person") { Person.new "Matt", "Buck" }
-  describe ".initialize" do
-    it "creates a new person with a first and last name" do
+  context "when a name is provided" do
+    let("person") { Person.new "Matt", "Buck" }
+
+    describe ".initialize" do
+      it "creates a new person with a first and last name" do
+        # person = Person.new "Matt", "Buck"
+        expect(person.first_name).to eq("Matt")
+        expect(person.last_name).to eq("Buck")
+      end
+    end  
+
+    describe "#full_name" do
+      it "returns the full name of the person" do
       # person = Person.new "Matt", "Buck"
-      expect(person.first_name).to eq("Matt")
-      expect(person.last_name).to eq("Buck")
+        expect(person.full_name).to eq("Matt Buck")
+      end
+    end
+
+    describe "reverse_name" do 
+      it "returns the reverse name of a person" do 
+        expect(person.reverse_name).to eq("kcuB ttaM")
+      end
+      
+    end
+  end   
+
+ context "when a name is not provided" do
+    let("person") { Person.new}
+
+    describe ".initialize" do
+      it "creates a person without a first or last name" do
+        expect(person.first_name).to be_nil
+        expect(person.last_name).to be_nil
+      end
+    end
+    
+    describe "#full_name" do 
+      it "returns nil" do
+        expect(person.full_name).to be_nil
+      end
     end
   end  
+end
 
-describe "#full_name" do
-  it "returns the full name of the person" do
-    # person = Person.new "Matt", "Buck"
-    expect(person.full_name).to eq("Matt Buck")
-   end
-  end
-end   
 
 
 # bundle exec rspec -f d -c person_spec.rb
